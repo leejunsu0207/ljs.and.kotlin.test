350835b032baf006474f6d51e2457858e644f3dd
leejunsu <qkfhtit@gmail.com>
2021-11-19 15:41:00 +0900

    fun

--- app/src/main/java/com/ljs/and/controlflow1/MainActivity.kt
+++ app/src/main/java/com/ljs/and/controlflow1/MainActivity.kt
@@ -268,12 +268,49 @@ class MainActivity : AppCompatActivity() {
 
 
 
+        // functin
+
+        // 4. 반환값이 있는 함수 square 사용하기
+        var squareResult = square(30)
+        Log.d("fun", "30의 제곱은 ${squareResult}입니다.")
+
+        // 5. 반환값이 없는 함수는 그냥 실행
+        printSum(3, 5)
+
+        // 6. 입력값이 없는 함수 사용하기
+        val PI = getPi()
+        Log.d("fun", "지름이 10인 원의 둘레는 ${10 * PI}")
 
 
 
 
 
 
+
+
+    }
+
+
+    // 1. 반환값이 있는 함수
+    fun square(x :Int):Int{
+        return x * x
+    }
+
+    // 2. 반환값이 없는 함수
+    fun printSum(x: Int, y:Int){
+        Log.d("fun", "x + y = ${x + y}")
+    }
+
+    // 3. 입력값 없이 반환값만 이쓴ㄴ 함수
+    fun getPi(): Double{
+        return 3.14
+    }
+
+    // 7. 기본값을 갖는 함수
+    fun newFunction(name:String, age:Int = 29, weight:Double = 65.5){
+        Log.d("fun", "name의 값은 ${name}")
+        Log.d("fun", "age의 값은 ${age}")
+        Log.d("fun", "weight의 값은 ${weight}")
 
     }
 }
